136ba77743f116d7a18741c09bf839f8372463d7
Viranti 119140024 <83486824+Viranti119140024@users.noreply.github.com>
2023-07-27 07:36:19 +0700

    login

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -37,6 +37,7 @@
                     <div class="form-group">
                         <label for="InputPassword" style="color: white; font-weight:bold;">Password</label>
                         <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+                        <span class="password-icon" style="color: black;" id="togglePassword">&#128065;</span>
                     </div>
                     <button type="submit" class="btn btn-masuk btn-block">Masuk</button>
                     <div class="form-footer mt-2">
@@ -52,6 +53,17 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+    <script>
+    const togglePassword = document.getElementById('togglePassword');
+    const passwordInput = document.getElementById('password');
+
+    togglePassword.addEventListener('click', function () {
+      const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+      passwordInput.setAttribute('type', type);
+      this.textContent = type === 'password' ? '\u{1F441}' : '\u{1F441}\u{1F5E0}';
+    });
+  </script>
 </body>
 
 </html>
