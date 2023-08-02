03331919defe6eab49724557b94cdefc845d8b2e
Viranti 119140024 <83486824+Viranti119140024@users.noreply.github.com>
2023-08-03 03:35:30 +0700

    update

--- application/views/templates/admin/dokumen/pilihawaldokumen.php
+++ application/views/templates/admin/dokumen/pilihawaldokumen.php
@@ -1,4 +1,3 @@
-
 <div class="container">
 
   <div class="card-deck">
@@ -18,14 +17,14 @@
         <a href="<?= base_url('user/dokprodi') ?>" class="btn" type="button" style="background-color: #DAA520; color:white">Lihat Detail</a>
       </div>
     </div>
-    <div class="card">
+    <!-- <div class="card">
       <div class="card-body">
         <h5 class="card-title">Dokumen Kebutuhan Audit</h5>
         <p class="card-text">Unit</p>
         <a href="<?= base_url('user/dokunit') ?>" class="btn" type="button" style="background-color: #DAA520; color:white">Lihat Detail</a>
 
       </div>
-    </div>
+    </div> -->
 
   </div>
 </div>
